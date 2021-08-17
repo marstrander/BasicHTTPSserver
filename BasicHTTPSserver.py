7fa56bb555e653577bee818d65f7a15d705a1de8
marstrander <40351427+marstrander@users.noreply.github.com>
2021-08-17 10:34:36 +0200

    Update BasicHTTPSserver.py

--- BasicHTTPSserver.py
+++ BasicHTTPSserver.py
@@ -1,24 +1,19 @@
-'''
-Basic HTTPS Server for testing purposes.
-Do not use in production.
-'''
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import ssl
 import sys
 
-if len(sys.argv) < 4:
-    print('\n(*) USAGE:\t python3 BasicHTTPSserver.py server_ip server_port key certificate')
-    print('(*) Example:\t python3 BasicHTTPSserver.py 10.1.10.99 4443 cert.key cert.pem\n')
+if len(sys.argv) < 5:
+    print('\n(*) USAGE:\t python3 %s server_ip server_port key certificate' % sys.argv[0])
+    print('(*) Example:\t python3 %s 10.1.10.99 443 cert.key cert.pem\n' % sys.argv[0])
     sys.exit()
 
-httpd = HTTPServer((sys.argv[1], int(sys.argv[2])), SimpleHTTPRequestHandler)
-
-httpd.socket = ssl.wrap_socket (httpd.socket,
+handler = HTTPServer((sys.argv[1], int(sys.argv[2])), SimpleHTTPRequestHandler)
+handler.socket = ssl.wrap_socket (handler.socket,
         keyfile=sys.argv[3],
         certfile=sys.argv[4], server_side=True)
 
-print('\n' + '-' * 80 + '\n')
-print('Server running on https://' + sys.argv[1] + ':' + str(sys.argv[2]) + '\n')
-print('-' * 80 + '\n')
-
-httpd.serve_forever()
+with handler as httpd:
+    print('\n' + '-' * 80 + '\n')
+    print('Server running on https://' + sys.argv[1] + ':' + str(sys.argv[2]) + '\n')
+    print('-' * 80 + '\n')
+    httpd.serve_forever()
