a30543011d7a92464ebe7f93cf975e3fe9e6bb60
marstrander <40351427+marstrander@users.noreply.github.com>
2021-09-07 13:01:00 +0200

    Update BasicHTTPSserver.py
    updated with proper string substitution

--- BasicHTTPSserver.py
+++ BasicHTTPSserver.py
@@ -3,8 +3,8 @@ import ssl
 import sys
 
 if len(sys.argv) < 5:
-    print('\n(*) USAGE:\t python3 %s server_ip server_port key certificate' % sys.argv[0])
-    print('(*) Example:\t python3 %s 10.1.10.99 443 cert.key cert.pem\n' % sys.argv[0])
+    print('\n(*) USAGE:\t python3 {0} server_ip server_port key certificate'.format(sys.argv[0]))
+    print('(*) Example:\t python3 {0} 10.1.10.99 443 cert.key cert.pem\n'.format(sys.argv[0]))
     sys.exit()
 
 handler = HTTPServer((sys.argv[1], int(sys.argv[2])), SimpleHTTPRequestHandler)
@@ -14,6 +14,6 @@ handler.socket = ssl.wrap_socket (handler.socket,
 
 with handler as httpd:
     print('\n' + '-' * 80 + '\n')
-    print('Server running on https://' + sys.argv[1] + ':' + str(sys.argv[2]) + '\n')
+    print('Server running on https://{0}{1}\n'.format(sys.argv[1], sys.argv[2]))
     print('-' * 80 + '\n')
     httpd.serve_forever()
